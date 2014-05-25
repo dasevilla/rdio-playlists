a01a503054d9c22d75e44d8b8552bf2d6bd2dd69
Devin Sevilla <devin.sevilla@rd.io>
2014-05-24 22:20:04 -0700

    Remove comment

--- rdiosite/settings.py
+++ rdiosite/settings.py
@@ -84,7 +84,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
